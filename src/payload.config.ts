4261b39fdd1592e3596127ccc2885f99e8d8cb91
YagoLopez <yago.lopez@gmail.com>
2025-10-10 00:12:17 +0200

    enable graphql playground

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -100,4 +100,7 @@ export default buildConfig({
   // if you want to resize images, crop, set focal point, etc.
   // make sure to install it and pass it to the config.
   // sharp,
+  graphQL: {
+    disablePlaygroundInProduction: false,
+  }
 })
