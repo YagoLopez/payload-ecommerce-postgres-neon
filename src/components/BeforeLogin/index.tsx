5c7fe60c3f874ddd20a6991867d242d03f992a51
YagoLopez <yago.lopez@zaelot.com>
2025-11-04 20:18:15 +0100

    Add guest credentials in admin login screen

--- src/components/BeforeLogin/index.tsx
+++ src/components/BeforeLogin/index.tsx
@@ -1,13 +1,34 @@
 import React from 'react'
 
+const styles = {
+  background: 'aliceblue',
+  borderWidth: '1px',
+  borderStyle: 'solid',
+  borderColor: 'navy',
+  borderRadius: '5px',
+  padding: '10px',
+  marginTop: '10px',
+  marginBottom: '10px',
+  boxShadow: '0 10px 8px rgba(0, 0, 0, 0.1)',
+}
+
 export const BeforeLogin: React.FC = () => {
   return (
     <div>
       <p>
-        <b>Welcome to your dashboard!</b>
-        {' This is where site admins will log in to manage your store. Customers will need to '}
-        <a href={`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/login`}>log in to the site instead</a>
-        {' to access their user account, order history, and more.'}
+        <b>Welcome to the Ecommerce Admin Dashboard.</b> This is where site admins will log in to manage the
+        store.
+      </p>
+      <div style={styles}>
+        <p>You can login as a guest using the following credentials:</p>
+        <p>➡️ <b>Email:</b> readonly@readonly.com</p>
+        <p>➡️ <b>Password:</b> readonly</p>
+      </div>
+
+      <p>
+        Customers will need to
+        <a href={`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/login`}> log in to the site instead </a>
+        to access their user account, order history, and more.<br/>
       </p>
     </div>
   )
