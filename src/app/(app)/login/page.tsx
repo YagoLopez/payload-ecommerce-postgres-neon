2b03950add48977e775193eb929ee8d2f48d11e2
YagoLopez <yago.lopez@gmail.com>
2025-11-07 20:50:22 +0100

    chore: add text to store login screen

--- src/app/(app)/login/page.tsx
+++ src/app/(app)/login/page.tsx
@@ -10,6 +10,17 @@ import { getPayload } from 'payload'
 import { LoginForm } from '@/components/forms/LoginForm'
 import { redirect } from 'next/navigation'
 
+const styles = {
+  background: 'aliceblue',
+  borderWidth: '1px',
+  borderStyle: 'solid',
+  borderColor: 'navy',
+  borderRadius: '5px',
+  padding: '10px',
+  marginBottom: '10px',
+  boxShadow: '0 10px 8px rgba(0, 0, 0, 0.1)',
+}
+
 export default async function Login() {
   const headers = await getHeaders()
   const payload = await getPayload({ config: configPromise })
@@ -24,11 +35,26 @@ export default async function Login() {
       <div className="max-w-xl mx-auto my-12">
         <RenderParams />
 
-        <h1 className="mb-4 text-[1.8rem]">Log in</h1>
-        <p className="mb-8">
-          {`This is where your customers will login to manage their account, review their order history, and more. To manage all users, `}
-          <Link href="/admin/collections/users">login to the admin dashboard</Link>.
+        <h1 className="mb-6 text-[1.8rem]">Log in</h1>
+        <p className="mb-2">
+          This is where the customers can login to the Ecommerce Store.
+
         </p>
+        <p className="mb-2">
+          To access the admin dashboard, please
+          <Link className="underline hover:underline-offset-4 text-blue-900" href="/admin/collections/users"> login here</Link>.
+
+        </p>
+        <div className="mb-10">
+          <div style={styles}>
+            <p style={{ color: 'navy', fontWeight: 'bold' }}>You can login as a guest using the following
+              credentials:</p>
+            <p>➡️ <b>Email:</b> readonly@readonly.com</p>
+            <p>➡️ <b>Password:</b> readonly</p>
+          </div>
+
+        </div>
+
         <LoginForm />
       </div>
     </div>
